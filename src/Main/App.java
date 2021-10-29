fa6675a1f80e14131c91e7993dd3a4ca4061ec91
BuildTools <unconfigured@null.spigotmc.org>
2021-10-29 15:44:47 +0200

    Programmed for someone else - v1.0

--- src/Main/App.java
+++ src/Main/App.java
@@ -13,10 +13,13 @@ public class App {
     private JPanel mainPanel;
     private JTextField textField;
     private JTextField input;
+    private JButton btn_stop;
 
     private boolean autoClickerRun = false;
     private int counter = 0;
     private int checkForInt = 0;
+
+    Thread acThread;
 
     public static void main(String[] args) {
         JFrame jframe = new JFrame("AutoClicker - v1.0");
@@ -36,6 +39,7 @@ public class App {
     @Deprecated
     public App() {
         btn_start.addActionListener(e -> {
+
             if (!input.getText().isEmpty()) {
                 try {
                     checkForInt = Integer.parseInt(input.getText());
@@ -47,29 +51,56 @@ public class App {
                             interruptedException.printStackTrace();
                         }
                         autoClickerRun = true;
-                        while (autoClickerRun) {
-                            try {
-                                Robot robot = new Robot();
-                                robot.mousePress(InputEvent.BUTTON1_MASK);
-                                robot.mouseRelease(InputEvent.BUTTON1_MASK);
-                                TimeUnit.MILLISECONDS.sleep(10);
-                            } catch (AWTException | InterruptedException awtException) {
-                                awtException.printStackTrace();
-                            }
-                            counter++;
-                            if (counter == Integer.parseInt(input.getText())) {
-                                autoClickerRun = false;
-                                counter = 0;
-                            }
-                        }
+                        enableAutoClicker();
                     }
                 } catch (NumberFormatException numberformatexception) {
-                    JOptionPane.showMessageDialog(null, "Eingabe muss eine Zahl sein!");
+                    JOptionPane optionPane = new JOptionPane("Eingabe muss eine Zahl sein!", JOptionPane.ERROR_MESSAGE);
+                    JDialog dialog = optionPane.createDialog("Eingabefehler");
+                    dialog.setAlwaysOnTop(true);
+                    dialog.setVisible(true);
                     textField.setSelectedTextColor(Color.RED);
                 }
             } else {
-                JOptionPane.showMessageDialog(null, "Zahl eintragen");
+                JOptionPane optionPane = new JOptionPane("Zahl eintragen", JOptionPane.ERROR_MESSAGE);
+                JDialog dialog = optionPane.createDialog("Eingabefehler");
+                dialog.setAlwaysOnTop(true);
+                dialog.setVisible(true);
             }
         });
+
+        btn_stop.addActionListener(e -> {
+            if (autoClickerRun) {
+                autoClickerRun = false;
+                try {
+                    if (acThread.isAlive()) {
+                        acThread.join();
+                    }
+                } catch (InterruptedException ex) {
+                    ex.printStackTrace();
+                }
+            }
+        });
+    }
+
+    @Deprecated
+    public void enableAutoClicker() {
+        acThread = new Thread(() -> {
+            while (autoClickerRun) {
+                try {
+                    Robot robot = new Robot();
+                    robot.mousePress(InputEvent.BUTTON1_MASK);
+                    robot.mouseRelease(InputEvent.BUTTON1_MASK);
+                    TimeUnit.MILLISECONDS.sleep(10);
+                } catch (AWTException | InterruptedException awtException) {
+                    awtException.printStackTrace();
+                }
+                counter++;
+                if (counter == Integer.parseInt(input.getText())) {
+                    autoClickerRun = false;
+                    counter = 0;
+                }
+            }
+        });
+        acThread.start();
     }
 }
